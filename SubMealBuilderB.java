2f3c592509b5ffd15e405576a57148bfd093a9dc
ChenYang135 <ChenYang135@users.noreply.github.com>
2017-05-07 22:06:01 +0800

    Create SubMealBuilderB.java

---
+++ SubMealBuilderB.java
@@ -0,0 +1,17 @@
+//具体建造者B-生产B套餐
+public class SubMealBuilderB extends MealBuilder
+{
+	public void buildFood()
+	{
+		meal.setFood("一个鸡肉卷");
+	}
+	public void buildDrink()
+	{
+		 meal.setDrink("一杯果汁");
+	}
+}
+
+
+
+
+
