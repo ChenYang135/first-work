fb134b2f685e534eebcee8f85af1c465fcde8ad0
ChenYang135 <ChenYang135@users.noreply.github.com>
2017-05-07 21:51:40 +0800

    -- 建造者类

---
+++ KFCWaiter.java
@@ -0,0 +1,15 @@
+//服务员（相当于建造者）
+public class KFCWaiter
+{
+	private MealBuilder mb;
+	public void setMealBuilder(MealBuilder mb)
+	{
+		this.mb=mb;
+	}
+	public Meal construct()
+	{
+		mb.buildFood();
+		mb.buildDrink();
+		return mb.getMeal();
+	}
+}
